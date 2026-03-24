4ea64911a46823e2aa7adcfd18d0b9331604e67c
deku <shivang.sharma23b@iiitg.ac.in>
2026-03-24 09:40:31 +0530

    updated frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,22 +5,26 @@ import ZoneDetails from "./pages/ZoneDetails";
 import Predictions from "./pages/Predictions";
 import ChatbotPage from "./pages/ChatbotPage";
 import Login from "./pages/Login";
+import LandingPage from "./pages/LandingPage";
+import SearchResults from "./pages/SearchResults";
 import Navbar from "./components/Navbar";
 
 function AppLayout() {
   const { pathname } = useLocation();
-  const hideNav = pathname === "/login";
+  const hideNav = pathname === "/login" || pathname === "/";
 
   return (
     <div style={{ minHeight: "100vh", background: "var(--bg)", color: "var(--text-1)" }}>
       {!hideNav && <Navbar />}
       <main style={{ paddingTop: hideNav ? 0 : "64px" }}>
         <Routes>
-          <Route path="/"           element={<Dashboard />} />
-          <Route path="/zone/:id"   element={<ZoneDetails />} />
-          <Route path="/predictions" element={<Predictions />} />
-          <Route path="/chatbot"    element={<ChatbotPage />} />
-          <Route path="/login"      element={<Login />} />
+          <Route path="/"                element={<LandingPage />} />
+          <Route path="/dashboard"       element={<Dashboard />} />
+          <Route path="/zone/:id"        element={<ZoneDetails />} />
+          <Route path="/predictions"     element={<Predictions />} />
+          <Route path="/chatbot"         element={<ChatbotPage />} />
+          <Route path="/login"           element={<Login />} />
+          <Route path="/search-results"  element={<SearchResults />} />
         </Routes>
       </main>
     </div>
